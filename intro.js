a87f5d3eb5868e541a4b2bd7e90aeff1e6fbede4
raswant <rashwanth92@gmail.com>
2021-06-10 11:03:26 -0700

    first commit

---
+++ intro.js
@@ -0,0 +1,32 @@
+// console.log("hello world")
+
+var number = 2
+// if(number == 0)
+// {
+//     console.log("true")
+// }
+// else
+// {
+//     console.log("false")
+// }
+
+switch(number)
+{
+    
+    case 1:
+    if(number == 4)
+    {
+        console.log("its 4")
+    }
+    break;
+
+    case 2:
+    if(number == 2)
+    {
+        console.log("it's 3")
+    }
+    break;
+
+    default:
+        console.log("no number matching")
+}
